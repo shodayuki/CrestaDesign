82cd1370f261f14db40267c6c461583566bdd0fc
shodayuki <yshoda1227@gmail.com>
2021-02-17 22:42:06 +0900

    フェードインを追加

--- js/index.js
+++ js/index.js
@@ -21,11 +21,36 @@ $(function () {
     }
   });
 
+  $(window).on('load', function () {
+    $('.slick-slide').find('img').each(function () {
+      $(this).addClass('is-active');
+    });
+
+    $('.jsc-fadein-text').each(function () {
+      $(this).addClass('is-active');
+    });
+  });
+
   $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 2000,
     speed: 1000,
     fade: true,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    swipe: false,
+    touchMove: false
+  });
+
+  $(window).on('scroll', function(){
+    const scroll = $(window).scrollTop();
+    const windowHeight = $(window).height();
+
+    $('.jsc-fadeup').each(function(){
+      const elemPos = $(this).offset().top;
+
+      if (scroll > elemPos - windowHeight + 100) {
+        $(this).addClass('scrollin');
+      }
+    });
   });
 });
