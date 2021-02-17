9abb7085fe87cc5a2d8fe4078649e45c82af6df0
shodayuki <yshoda1227@gmail.com>
2021-02-17 20:55:41 +0900

    メインページ実装

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-$(function(){
+$(function () {
   $('.header__burger-btn').on('click', function () {
     $('.header__nav').fadeToggle(300);
     $('.header__burger-btn').toggleClass('cross');
     $('body').toggleClass('noscroll');
+  });
+
+  const $fv = $('.fv');
+  const $header = $('.header');
+  const $fvHeight = $fv.outerHeight();
+
+  $(window).on('load scroll', function () {
+    const value = $(this).scrollTop();
+
+    if ($(window).width() > 768) {
+      if (value > $fvHeight) {
+        $header.addClass('is-fixed');
+      } else {
+        $header.removeClass('is-fixed');
+      }
+    }
+  });
+
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear'
   });
 });
